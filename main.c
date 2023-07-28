e201855fadd9e8fab7a4b96356b731e57e042cf3
Luanny Medeiros Lucena <lmedeiro@c3r7p5.42sp.org.br>
2023-07-28 17:52:29 -0300

    feat(main): add ctrl_c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lmedeiro <lmedeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:47:34 by lmedeiro          #+#    #+#             */
-/*   Updated: 2023/07/28 17:10:47 by lmedeiro         ###   ########.fr       */
+/*   Updated: 2023/07/28 17:48:35 by lmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int main(int argc, char **argv, char **envp)
 {
     char *input_line;
+    signal(SIGQUIT, sigquit_handler);
+    signal(SIGINT, sigint_handler);
 
     while(1)
     {
@@ -25,12 +27,12 @@ int main(int argc, char **argv, char **envp)
         ctrl_d(input_line);
         
         // Verificar se o usuário digitou "exit" para sair do minishell
-        ft_exit(input_line);
-
-        // Verificar se o usuário digitou (CTRL+ \) /
-        signal(SIGQUIT, sigquit_handler);
-
-        
+        if(strcmp(input_line, "exit") == 0)
+        {
+            printf("Bye, Bye!\n");
+            free(input_line);
+            exit(EXIT_SUCCESS);
+        }
         if(input_line)
         {
             printf("%s\n", input_line);    
