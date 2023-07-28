9a24be41faf26a99cab031522d3acdfc31d0f856
Luanny Medeiros Lucena <lmedeiro@c3r7p5.42sp.org.br>
2023-07-28 17:54:08 -0300

    feat(signals): add ctrl_c

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: lmedeiro <lmedeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:41:03 by lmedeiro          #+#    #+#             */
-/*   Updated: 2023/07/28 17:11:34 by lmedeiro         ###   ########.fr       */
+/*   Updated: 2023/07/28 17:47:00 by lmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,14 @@ void ctrl_d(char *input_line)
     }
 }
 
-void ft_exit(char *input_line)
-{
-    if(strcmp(input_line, "exit") == 0)
-    {
-        printf("Bye, Bye!\n");
-        free(input_line);
-        exit(EXIT_SUCCESS);
-    }
-}
-
 void sigquit_handler(int signal)
 {
     printf("Você pressionou Ctrl + \\, mas esse sinal foi ignorado.\n");
+}
+
+void sigint_handler(int signal)
+{
+    printf("Você pressionou Ctrl + C, mas esse sinal foi ignorado.\n");
+    rl_on_new_line();
+    rl_redisplay();
 }
