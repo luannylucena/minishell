4858b7d7ec259916f30360dd98596869d6039ad3
Luanny Medeiros Lucena <lmedeiro@c3r5p1.42sp.org.br>
2023-08-02 16:00:52 -0300

    feat(signals): criando pasta

---
+++ signals/signals.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmedeiro <lmedeiro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/28 14:41:03 by lmedeiro          #+#    #+#             */
+/*   Updated: 2023/08/02 12:53:10 by lmedeiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+#include "minishell.h"
+ 
+
+void ctrl_d(char *input_line)
+{
+    if(input_line == NULL)
+    {
+        printf("Você digitou ctrl+d, adeus!\n");
+        exit(EXIT_SUCCESS);
+    }
+}
+
+void sigquit_handler(int signal)
+{
+    printf("\rVocê pressionou Ctrl + \\, mas esse sinal foi ignorado.\n");
+    rl_on_new_line();
+    rl_redisplay();
+    
+}
+
+void sigint_handler(int signal)
+{
+    printf("\rVocê pressionou Ctrl + C, mas esse sinal foi ignorado.\n");
+    rl_on_new_line();
+    rl_redisplay();
+}
+
