9776ddd73dfafce75ed90c5a15411a06395aee7a
Luanny Medeiros Lucena <lmedeiro@c1r5p2.42sp.org.br>
2023-08-15 15:19:56 -0300

    feat(main): implementando

--- main.c
+++ main.c
@@ -6,43 +6,39 @@
 /*   By: lmedeiro <lmedeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:47:34 by lmedeiro          #+#    #+#             */
-/*   Updated: 2023/07/28 17:48:35 by lmedeiro         ###   ########.fr       */
+/*   Updated: 2023/08/15 15:19:09 by lmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-    char *input_line;
-    signal(SIGQUIT, sigquit_handler);
-    signal(SIGINT, sigint_handler);
+	char	*input_line;
 
-    while(1)
-    {
-        // Exibir o prompt e aguardar um comando usando readline
-        input_line = readline("minishell > ");
-
-        // Verificar se o usuário digitou CTRL+D
-        ctrl_d(input_line);
-        
-        // Verificar se o usuário digitou "exit" para sair do minishell
-        if(strcmp(input_line, "exit") == 0)
-        {
-            printf("Bye, Bye!\n");
-            free(input_line);
-            exit(EXIT_SUCCESS);
-        }
-        if(input_line)
-        {
-            printf("%s\n", input_line);    
-            // Adicionando o comando ao histórico do readline
-            if (strlen(input_line) > 0) //adaptar ao ft_strlen
-            {
-                add_history(input_line);
-            }
-            free(input_line);
-        }
-    }
-    return(0);
+	signal (SIGQUIT, sigquit_handler);
+	signal (SIGINT, sigint_handler);
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	while (1)
+	{
+		input_line = readline("minishell > ");
+		parser(input_line);
+		ctrl_d(input_line);
+		if (strcmp(input_line, "exit") == 0)
+		{
+			printf ("Bye, Bye!\n");
+			free (input_line);
+			exit (EXIT_SUCCESS);
+		}
+		if (input_line)
+		{
+			printf ("%s\n", input_line);
+			if (ft_strlen(input_line) > 0)
+				add_history(input_line);
+			free(input_line);
+		}
+	}
+	return (0);
 }
