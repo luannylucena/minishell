fff522043908727a44203919c77d3d1c2b0c0fbe
Luanny Medeiros Lucena <lmedeiro@c1r5p2.42sp.org.br>
2023-08-15 15:13:43 -0300

    feat(signals): implementando signals

--- signals/signals.c
+++ signals/signals.c
@@ -6,39 +6,33 @@
 /*   By: lmedeiro <lmedeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:41:03 by lmedeiro          #+#    #+#             */
-/*   Updated: 2023/08/02 16:17:52 by lmedeiro         ###   ########.fr       */
+/*   Updated: 2023/08/15 15:07:35 by lmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <signal.h>
-#include "minishell.h"
- 
-//
-void ctrl_d(char *input_line)
-{
-    if(input_line == NULL)
-    {
-        printf("Você digitou ctrl+d, adeus!\n");
-        exit(EXIT_SUCCESS);
-    }
+#include "../includes/minishell.h"
+
+void	ctrl_d(char *input_line)
+{	
+	if (input_line == NULL)
+	{
+		printf("Você digitou ctrl+d, adeus!\n");
+		exit(EXIT_SUCCESS);
+	}
 }
 
-void sigquit_handler(int signal)
+void	sigquit_handler(int signal)
 {
-    printf("\rVocê pressionou Ctrl + \\, mas esse sinal foi ignorado.\n");
-    rl_on_new_line();
-    rl_redisplay();
-    
+	printf("\rVocê pressionou Ctrl + \\, mas esse sinal foi ignorado.\n");
+	rl_on_new_line();
+	rl_redisplay();
+	(void)signal;
 }
 
-void sigint_handler(int signal)
+void	sigint_handler(int signal)
 {
-    printf("\rVocê pressionou Ctrl + C, mas esse sinal foi ignorado.\n");
-    rl_on_new_line();
-    rl_redisplay();
+	printf("\rVocê pressionou Ctrl + C, mas esse sinal foi ignorado.\n");
+	rl_on_new_line();
+	rl_redisplay();
+	(void)signal;
 }
-
